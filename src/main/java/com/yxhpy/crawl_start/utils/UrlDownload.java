c32533a0cad875771837efe728878f19aa9f0831
yxhpy <1764807112@qq.com>
2024-08-27 18:02:28 +0800

    # 优化 爬虫器性能

--- src/main/java/com/yxhpy/crawl_start/utils/UrlDownload.java
+++ src/main/java/com/yxhpy/crawl_start/utils/UrlDownload.java
@@ -3,9 +3,9 @@ package com.yxhpy.crawl_start.utils;
 import com.yxhpy.crawl_start.entity.ParseHtmlValueDTO;
 import com.yxhpy.crawl_start.entity.RequestUrlValueDTO;
 import io.reactivex.rxjava3.core.Observable;
-import io.reactivex.rxjava3.disposables.Disposable;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.Synchronized;
+import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
@@ -13,77 +13,96 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-
+@Slf4j
 @Component
 public class UrlDownload {
     static OkHttpClient client = null;
 
+    static {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        client = builder
+                .callTimeout(3, TimeUnit.SECONDS)
+                .build();
+    }
+
+    private final Semaphore semaphore = new Semaphore(10); // 限制并发请求数
+
     public Observable<RequestUrlValueDTO> request(String url) {
         return Observable.create((emitter -> {
-            Request.Builder builder = new Request.Builder();
-            Request request = builder.url(url)
-                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
-                    .get().build();
-            client.newCall(request).enqueue(new Callback() {
-                @Override
-                public void onResponse(@NotNull Call call, @NotNull Response response) {
-                    try (response; response) {
-                        ResponseBody body = response.body();
-                        if (body != null) {
-                            RequestUrlValueDTO build = RequestUrlValueDTO.builder()
-                                    .html(body.string())
-                                    .url(url)
-                                    .build();
-                            emitter.onNext(build);
-                            emitter.onComplete();
+            try {
+                semaphore.acquire();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(url)
+                        .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36")
+                        .get().build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) {
+                        try (ResponseBody body = response.body()) {
+                            if (body != null) {
+                                RequestUrlValueDTO build = RequestUrlValueDTO.builder()
+                                        .html(body.string())
+                                        .url(url)
+                                        .build();
+                                emitter.onNext(build);
+                                emitter.onComplete();
+                            }
+                        } catch (Exception e) {
+                            emitter.onError(e);
+                        } finally {
+                            semaphore.release();
                         }
-                    } catch (Exception e) {
-                        emitter.onError(e);
                     }
-                }
 
-                @Override
-                public void onFailure(@NotNull Call call, @NotNull IOException e) {
-                    emitter.onError(e);
-                }
-            });
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        emitter.onError(e);
+                        semaphore.release();
+                    }
+                });
+            } catch (InterruptedException e) {
+                emitter.onError(e);
+            }
         }));
     }
 
     public Observable<RequestUrlValueDTO> requestAndRetry(String url) {
         return request(url)
-                .retry(2)
-                .onErrorResumeNext(Observable::error);
+                .retryWhen(errors -> errors.zipWith(Observable.range(1, 3), (n, i) -> {
+                            log.debug("访问第{}次，发生错误:{}", i, n.getMessage());
+                            if (i >= 3) {
+                                throw n;
+                            }
+                            return i;
+                        })
+                        .flatMap(i -> Observable.timer((long) Math.pow(2, i), TimeUnit.SECONDS)))
+                .onErrorResumeNext(throwable -> {
+                    log.error("Error after retries for URL {}: {}", url, throwable.getMessage());
+                    return Observable.empty(); // 返回空 Observable 而不是错误
+                });
     }
+
 
     @Synchronized
     public List<RequestUrlValueDTO> run(List<String> urls) {
-        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-        client = builder
-                .callTimeout(10, TimeUnit.SECONDS)
-                .build();
         List<RequestUrlValueDTO> results = new CopyOnWriteArrayList<>();
-        Disposable completed = Observable.fromIterable(urls)
+        CountDownLatch latch = new CountDownLatch(urls.size());
+        Observable.fromIterable(urls)
                 .subscribeOn(Schedulers.io())
-                .flatMap(this::requestAndRetry, true)
+                .flatMap((url) -> this.requestAndRetry(url).doFinally(latch::countDown))
                 .observeOn(Schedulers.computation())
-                .subscribe((results::add), e -> {
-                    System.out.println("异常");
-                }, () -> {
-                    System.out.println("Completed");
-                });
-        while (!completed.isDisposed()) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+                .subscribe((results::add));
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Operation was interrupted", e);
         }
-        client.dispatcher().executorService().shutdown();
-        client.connectionPool().evictAll();
         return results;
     }
 
@@ -99,11 +118,11 @@ public class UrlDownload {
             HtmlParse htmlParse = new HtmlParse();
             List<ParseHtmlValueDTO> run1 = htmlParse.run(run);
             for (ParseHtmlValueDTO parseHtmlValueDTO : run1) {
-                System.out.printf("%s %s\n", parseHtmlValueDTO.getTitle(), parseHtmlValueDTO.getUrl());
+                log.debug("{} {}", parseHtmlValueDTO.getTitle(), parseHtmlValueDTO.getUrl());
             }
             collect = run1.stream().map(ParseHtmlValueDTO::getUrls).flatMap(List::stream).collect(Collectors.toList());
-            collect = collect.subList(0, Math.min(collect.size(), 100));
-            System.out.println(collect.size());
+            collect = collect.subList(0, Math.min(collect.size(), 500));
+            log.debug("获得子网站{}个", collect.size());
         }
     }
 }
